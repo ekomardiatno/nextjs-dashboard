28f33d639b7347f5a78cd7084b4e84a366aaffc7
Eko Mardiatno <ekomardiatno@gmail.com>
2025-02-02 17:50:26 +0700

    simple animation

--- app/invitation/page.tsx
+++ app/invitation/page.tsx
@@ -1,6 +1,6 @@
 'use client';
 import { useEffect, useMemo, useState } from 'react';
-import { sacramento, workSans } from '../ui/fonts';
+import { playball, workSans } from '../ui/fonts';
 import '../ui/invitation.css';
 import { HeartIcon } from '@heroicons/react/20/solid';
 import { EnvelopeOpenIcon } from '@heroicons/react/24/outline';
@@ -33,11 +33,31 @@ export default function InvitationPage() {
     return days + "d " + hours + "h " + minutes + "m " + seconds + "s"
   }, [currentTime, countDownDate])
 
+  useEffect(() => {
+    const sections = document.querySelectorAll(".invitation-body section");
+
+    const revealOnScroll = () => {
+      sections.forEach((section) => {
+        const sectionTop = section.getBoundingClientRect().top;
+        if (sectionTop < window.innerHeight - 100) {
+          section.classList.remove("opacity-0", "translate-y-10");
+        } else {
+          section.classList.add("opacity-0", "translate-y-10");
+        }
+      });
+    };
+
+    window.addEventListener("scroll", revealOnScroll);
+    revealOnScroll(); // Run initially
+
+    return () => window.removeEventListener("scroll", revealOnScroll);
+  }, []);
+
   return (
     <main className={workSans.className + " bg-slate-100 dark:bg-slate-800"}>
       <section id='hero' className="hero h-screen flex flex-col items-center justify-center">
         <h4 className='dark:text-white text-lg mb-2'>Kepada Bapak/Ibu/Saudara/i,</h4>
-        <h1 className={sacramento.className + ' text-pink-600 text-5xl mb-2'}>Shandika & Nofariza</h1>
+        <h1 className={playball.className + ' text-pink-600 text-5xl mb-2'}>Shandika & Nofariza</h1>
         <div className="mb-10 text-center">
           <p className='dark:text-white mb-4'>Akan melangsungkan resepsi pernikahan dalam:</p>
           <div className="flex gap-2 justify-center">
@@ -55,8 +75,8 @@ export default function InvitationPage() {
         </div>
         <a href='#invitation-home' className="bg-pink-400 text-pink-800 px-4 py-2 hover:bg-pink-500 hover:text-pink-900 active:text-pink-950 rounded-xl active:bg-pink-600 transition active:scale-95 hover:text"><EnvelopeOpenIcon className='w-5 h-5 align-middle inline-block' /><span className='align-middle ml-2'>Lihat Undangan</span></a>
       </section>
-      <div className='relative'>
-        <nav className="bg-pink-400 sticky top-0">
+      <div className='relative invitation-body'>
+        <nav className="bg-pink-400 sticky top-0 z-[1050]">
           <div className="mx-auto max-w-7xl px-2 sm:px-6 lg:px-8">
             <div className="relative flex h-16 items-center justify-between">
               <div className="absolute inset-y-0 left-0 flex items-center sm:hidden">
@@ -73,7 +93,7 @@ export default function InvitationPage() {
               </div>
               <div className="flex flex-1 items-center justify-center sm:items-stretch sm:justify-start">
                 <div className="flex shrink-0 items-center">
-                  <p className={sacramento.className + ' text-3xl text-white'}>emvite</p>
+                  <p className={playball.className + ' text-3xl text-white'}>emvite</p>
                 </div>
                 <div className="hidden sm:ml-6 sm:block">
                   <div className="flex space-x-4">
@@ -88,9 +108,9 @@ export default function InvitationPage() {
           </div>
         </nav>
 
-        <section id='invitation-home' className="min-h-screen flex flex-col justify-center -mt-16 pt-16">
+        <section id='invitation-home' className="min-h-screen flex flex-col justify-center -mt-16 pt-16 opacity-0 transform translate-y-10 transition-all duration-700">
           <div className="mx-auto max-w-2xl px-2 sm:px-6 lg:px-8 mb-24 mt-10">
-            <h2 className={sacramento.className + ' text-pink-600 text-5xl text-center mb-3'}>Acara Pernikahan</h2>
+            <h2 className={playball.className + ' text-pink-600 text-5xl text-center mb-3'}>Acara Pernikahan</h2>
             <h3 className='text-lg text-center dark:text-white mb-2'>Diselenggarakan pada 5 Jan 2030 di Pekanbaru.</h3>
             <p className='text-center dark:text-white'>Oleh karena itu, dengan segala hormat, kami bermaksud untuk mengundang Bapak/Ibu/Saudara/i, untuk hadir pada acara pernikahan kami.</p>
           </div>
@@ -100,7 +120,7 @@ export default function InvitationPage() {
               <div className='flex flex-1 gap-4 lg:flex-row-reverse'>
                 <div className="w-36 h-36 md:w-48 md:h-48 rounded-full bg-slate-400"></div>
                 <div className='flex-1'>
-                  <h3 className={sacramento.className + ' text-4xl font-bold mb-2 text-pink-600 text-left lg:text-right'}>Shandika Galih</h3>
+                  <h3 className={playball.className + ' text-4xl font-bold mb-2 text-pink-600 text-left lg:text-right'}>Shandika Galih</h3>
                   <p className='mb-2 text-gray-700 dark:text-gray-200 text-left lg:text-right'>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Repudiandae soluta voluptate molestiae fugit nisi ut ducimus eveniet a quisquam sunt.</p>
                   <p className='text-gray-700 dark:text-gray-200 text-left lg:text-right'>Putra dari Bpk. Lorem dan Ibu Ipsum</p>
                 </div>
@@ -110,7 +130,7 @@ export default function InvitationPage() {
               </div>
               <div className='flex flex-1 gap-4 lg:flex-row-reverse'>
                 <div className='flex-1'>
-                  <h3 className={sacramento.className + ' text-4xl font-bold mb-2 text-pink-600 text-right lg:text-left'}>Nofariza</h3>
+                  <h3 className={playball.className + ' text-4xl font-bold mb-2 text-pink-600 text-right lg:text-left'}>Nofariza</h3>
                   <p className='mb-2 text-gray-700 dark:text-gray-200 text-right lg:text-left'>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Assumenda dolorum, fugit sed magni suscipit non.</p>
                   <p className='text-gray-700 dark:text-gray-200 text-right lg:text-left'>Putra dari Bpk. Lorem dan Ibu Ipsum</p>
                 </div>
@@ -123,16 +143,16 @@ export default function InvitationPage() {
 
         {isOpen && (
           <div
-            className="fixed inset-0 bg-black bg-opacity-50"
+            className="fixed inset-0 bg-black bg-opacity-50 z-[1050]"
             onClick={() => setIsOpen(false)}
           ></div>
         )}
 
         <div
-          className={`fixed top-0 right-0 h-full w-64 bg-white dark:bg-slate-800 shadow-lg transform transition-transform duration-300 ease-in-out${!isOpen ? ' translate-x-full' : ' translate-x-0'}`}
+          className={`z-[1050] fixed top-0 right-0 h-full w-64 bg-white dark:bg-slate-800 shadow-lg transform transition-transform duration-300 ease-in-out ${!isOpen ? 'translate-x-full' : 'translate-x-0'}`}
         >
           <div className="p-4 flex justify-between items-center">
-            <p className={sacramento.className + ' text-3xl text-pink-600 dark:text-white'}>emvite</p>
+            <p className={playball.className + ' text-3xl text-pink-600 dark:text-white'}>emvite</p>
             <button onClick={() => setIsOpen(false)} className="text-xl dark:text-white">
               &times;
             </button>
--- app/ui/fonts.ts
+++ app/ui/fonts.ts
@@ -1,6 +1,7 @@
-import { Inter, Lusitana, Sacramento, Work_Sans } from 'next/font/google'
+import { Inter, Lusitana, Sacramento, Work_Sans, Playball } from 'next/font/google'
 
 export const inter = Inter({ subsets: ['latin'] })
 export const lusitana = Lusitana({ subsets: ['latin'], weight: ['400', '700'] })
 export const sacramento = Sacramento({ subsets: ['latin'], weight: ['400'] })
 export const workSans = Work_Sans({ subsets: ['latin'], weight: ['100', '400', '600', '700'] })
+export const playball = Playball({ subsets: ['latin'], weight: ['400'] })
